38a450e603b5b4e848e8c1007b418a1b0a1309e5
Anmol <anmolshinde206@gmail.com>
2026-03-30 20:04:42 +0530

    U

--- src/main/java/com.complaint.system/ComplaintController.java
+++ src/main/java/com.complaint.system/ComplaintController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ComplaintController {
 
-    @GetMapping("/dashboard")
+    @GetMapping("/")
     public String showDashboard(Model model) {
         model.addAttribute("title", "Intelligent Complaint System");
         return "index"; // This looks for dashboard.html in templates
