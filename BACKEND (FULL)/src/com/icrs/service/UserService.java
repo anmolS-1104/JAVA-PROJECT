6b39589043aa59fb0c7528a8d5d707298041872b
amaan <amaansaxena.13@gmail.com>
2026-04-08 23:41:46 +0530

    Separate user and agent login/register into distinct classes

--- BACKEND (FULL)/src/com/icrs/service/UserService.java
+++ BACKEND (FULL)/src/com/icrs/service/UserService.java
@@ -9,18 +9,10 @@ public class UserService {
     private UserDAO userDAO = new UserDAOImpl();
 
     public boolean register(User user) {
-        if (user.getRole().equals("agent")) {
-            return userDAO.registerAgent(user);
-        } else {
-            return userDAO.registerUser(user);
-        }
+        return userDAO.registerUser(user);
     }
 
-    public User login(String email, String password, String role) {
-        if (role.equals("agent")) {
-            return userDAO.loginAgent(email, password);
-        } else {
-            return userDAO.loginUser(email, password);
-        }
+    public User login(String email, String password) {
+        return userDAO.loginUser(email, password);
     }
 }
