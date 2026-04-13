f30df70f6c4fa4d6743cbead3b54bb4f96358ed7
anmolS-1104 <anmolshinde2006@gmail.com>
2026-04-13 20:50:22 +0530

    Fx

--- src/main/java/com.complaint.system/ComplaintApplication.java
+++ src/main/java/com.complaint.system/ComplaintApplication.java
@@ -8,8 +8,18 @@ import javafx.stage.Stage;
 
 public class ComplaintApplication extends Application {
 
-@SpringBootApplication
-public class ComplaintApplication {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // This looks for the dashboard.fxml we put in src/main/resources
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/dashboard.fxml"));
+        Parent root = loader.load();
+
+        primaryStage.setTitle("ICRS - Intelligent Dashboard");
+        primaryStage.setScene(new Scene(root, 700, 500));
+
+        // This makes the window pop up!
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ComplaintApplication.class, args);
