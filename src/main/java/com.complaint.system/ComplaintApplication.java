0258337cdf770d2e388304c1ba5e378119c4e89f
anmolS-1104 <anmolshinde2006@gmail.com>
2026-04-13 20:50:30 +0530

    u

--- src/main/java/com.complaint.system/ComplaintApplication.java
+++ src/main/java/com.complaint.system/ComplaintApplication.java
@@ -22,20 +22,7 @@ public class ComplaintApplication extends Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ComplaintApplication.class, args);
-    }
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void launchBrowser() {
-        System.setProperty("java.awt.headless", "false");
-        String url = "http://localhost:8080/";
-        try {
-            if (Desktop.isDesktopSupported()) {
-                Desktop.getDesktop().browse(new URI(url));
-                System.out.println("Dashboard launched at: " + url);
-            }
-        } catch (Exception e) {
-            System.out.println("Could not launch browser. Visit: " + url);
-        }
+        // This starts the JavaFX lifecycle instead of the Spring Boot one
+        launch(args);
     }
 }
