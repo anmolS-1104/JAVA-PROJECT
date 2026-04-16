6a90fb9dda59542b363c4610ec6dded5d36213c4
anmolS-1104 <anmolshinde2006@gmail.com>
2026-04-16 16:07:46 +0530

    u

--- src/main/java/com.complaint.system/ComplaintController.java
+++ src/main/java/com.complaint.system/ComplaintController.java
@@ -5,6 +5,7 @@ import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
